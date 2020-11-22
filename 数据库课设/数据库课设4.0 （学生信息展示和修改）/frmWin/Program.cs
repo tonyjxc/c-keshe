b96bcd2540a5389747f438ff3de075d215e4946f
tonyjxc <tonyjxc@gmail.com>
2020-11-22 22:08:02 +0800

    学生信息4

---
+++ 数据库课设/数据库课设4.0 （学生信息展示和修改）/frmWin/Program.cs
@@ -0,0 +1,30 @@
+﻿using frmWin;
+using frmWin.ClassInfo;
+using frmWin.Student;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace keshe
+{
+    public class MyGlobal
+    {
+        public static string ip = "tonyjxc.xyz,6220";
+    }
+    static class Program
+    {
+
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmMain());
+        }
+    }
+}
