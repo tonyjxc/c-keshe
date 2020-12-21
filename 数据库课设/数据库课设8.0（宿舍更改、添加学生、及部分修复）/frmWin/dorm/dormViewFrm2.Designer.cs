9dc7c93d71de77ea398b193f90218e2171e5e531
tonyjxc <tonyjxc@gmail.com>
2020-12-21 09:53:45 +0800

    8.0

---
+++ 数据库课设/数据库课设8.0（宿舍更改、添加学生、及部分修复）/frmWin/dorm/dormViewFrm2.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace frmWin.dorm
+{
+    partial class dormViewFrm2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtFlag = new System.Windows.Forms.TextBox();
+            this.txtBuilding = new System.Windows.Forms.TextBox();
+            this.txtDormNum = new System.Windows.Forms.TextBox();
+            this.txtMemo = new System.Windows.Forms.TextBox();
+            this.txtPerson = new System.Windows.Forms.TextBox();
+            this.txtType = new System.Windows.Forms.TextBox();
+            this.txtDormId = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(491, 353);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "退出";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtFlag);
+            this.groupBox1.Controls.Add(this.txtBuilding);
+            this.groupBox1.Controls.Add(this.txtDormNum);
+            this.groupBox1.Controls.Add(this.txtMemo);
+            this.groupBox1.Controls.Add(this.txtPerson);
+            this.groupBox1.Controls.Add(this.txtType);
+            this.groupBox1.Controls.Add(this.txtDormId);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(70, 48);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(496, 275);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "更多信息";
+            // 
+            // txtFlag
+            // 
+            this.txtFlag.Enabled = false;
+            this.txtFlag.Location = new System.Drawing.Point(343, 154);
+            this.txtFlag.Name = "txtFlag";
+            this.txtFlag.Size = new System.Drawing.Size(136, 25);
+            this.txtFlag.TabIndex = 13;
+            // 
+            // txtBuilding
+            // 
+            this.txtBuilding.Enabled = false;
+            this.txtBuilding.Location = new System.Drawing.Point(343, 99);
+            this.txtBuilding.Name = "txtBuilding";
+            this.txtBuilding.Size = new System.Drawing.Size(136, 25);
+            this.txtBuilding.TabIndex = 12;
+            // 
+            // txtDormNum
+            // 
+            this.txtDormNum.Enabled = false;
+            this.txtDormNum.Location = new System.Drawing.Point(343, 34);
+            this.txtDormNum.Name = "txtDormNum";
+            this.txtDormNum.Size = new System.Drawing.Size(136, 25);
+            this.txtDormNum.TabIndex = 11;
+            // 
+            // txtMemo
+            // 
+            this.txtMemo.Enabled = false;
+            this.txtMemo.Location = new System.Drawing.Point(101, 202);
+            this.txtMemo.Name = "txtMemo";
+            this.txtMemo.Size = new System.Drawing.Size(131, 25);
+            this.txtMemo.TabIndex = 10;
+            // 
+            // txtPerson
+            // 
+            this.txtPerson.Enabled = false;
+            this.txtPerson.Location = new System.Drawing.Point(101, 154);
+            this.txtPerson.Name = "txtPerson";
+            this.txtPerson.Size = new System.Drawing.Size(131, 25);
+            this.txtPerson.TabIndex = 9;
+            // 
+            // txtType
+            // 
+            this.txtType.Enabled = false;
+            this.txtType.Location = new System.Drawing.Point(101, 106);
+            this.txtType.Name = "txtType";
+            this.txtType.Size = new System.Drawing.Size(131, 25);
+            this.txtType.TabIndex = 8;
+            // 
+            // txtDormId
+            // 
+            this.txtDormId.Enabled = false;
+            this.txtDormId.Location = new System.Drawing.Point(101, 34);
+            this.txtDormId.Name = "txtDormId";
+            this.txtDormId.Size = new System.Drawing.Size(131, 25);
+            this.txtDormId.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(270, 164);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 15);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "状态";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(270, 109);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "所属楼宇";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(270, 45);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "宿舍门号";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "备注";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "宿舍人数";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "宿舍类型";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "宿舍编号";
+            // 
+            // dormViewFrm2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 412);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "dormViewFrm2";
+            this.Text = "dormViewFrm2";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.dormViewFrm2_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtFlag;
+        private System.Windows.Forms.TextBox txtBuilding;
+        private System.Windows.Forms.TextBox txtDormNum;
+        private System.Windows.Forms.TextBox txtMemo;
+        private System.Windows.Forms.TextBox txtPerson;
+        private System.Windows.Forms.TextBox txtType;
+        private System.Windows.Forms.TextBox txtDormId;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
